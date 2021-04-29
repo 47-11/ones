0e098d3a4a6cdc84f47e59c51aff7f3b95d21012
Nikos Epping <nikos.epping@protonmail.com>
2021-04-29 15:59:27 +0200

    Allow * CORS origin for API endpoints

---
+++ ones-webapp/src/main/java/de/fourtyseveneleven/ones/security/configuration/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package de.fourtyseveneleven.ones.security.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration  implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        // Safe to allow for api endpoints, since we use JWT tokens.
+        registry.addMapping("/api/**")
+                .allowedOrigins("*");
+    }
+}
