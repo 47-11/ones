7e2a28a4515594d5f770ef79bd2f2295a55b57e5
Sam Nowakowski <sam.l.nowakowski@studmail.w-hs.de>
2021-04-30 21:49:01 +0200

    Added <form> tags with default submit

--- ones-frontend/src/views/auth/Login.vue
+++ ones-frontend/src/views/auth/Login.vue
@@ -1,36 +1,38 @@
 <template>
     <guest-layout>
         <auth-card>
-            <div class="px-6 py-5">
+            <form @submit.prevent="login">
+                <div class="px-6 py-5">
 
-                <feedback color="danger" class="mb-5" v-if="error.length > 0">
-                    <h2 class="text-lg">Oops! Etwas ist schief gelaufen.</h2>
-                    {{ error }}
-                </feedback>
+                    <feedback color="danger" class="mb-5" v-if="error.length > 0">
+                        <h2 class="text-lg">Oops! Etwas ist schief gelaufen.</h2>
+                        {{ error }}
+                    </feedback>
 
-                <div>
-                    <v-label>E-Mail</v-label>
-                    <v-input type="text" class="w-full" v-model="email"></v-input>
+                    <div>
+                        <v-label>E-Mail</v-label>
+                        <v-input type="text" class="w-full" v-model="email"></v-input>
+                    </div>
+
+                    <div class="mt-5">
+                        <v-label>Passwort</v-label>
+                        <v-input type="password" class="w-full" v-model="password"></v-input>
+                    </div>
+
+                    <div class="block mt-4">
+                        <label for="accept_policy_conditions" class="inline-flex items-center">
+                            <v-checkbox id="accept_policy_conditions" name="remember" v-model="remember"/>
+                            <span class="ml-2 text-sm text-gray-600">
+                                Sitzung merken und eingeloggt bleiben.
+                        </span>
+                        </label>
+                    </div>
                 </div>
 
-                <div class="mt-5">
-                    <v-label>Passwort</v-label>
-                    <v-input type="password" class="w-full" v-model="password"></v-input>
+                <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6">
+                    <v-button @click.native="login">Einloggen</v-button>
                 </div>
-
-                <div class="block mt-4">
-                    <label for="accept_policy_conditions" class="inline-flex items-center">
-                        <v-checkbox id="accept_policy_conditions" name="remember" v-model="remember"/>
-                        <span class="ml-2 text-sm text-gray-600">
-                            Sitzung merken und eingeloggt bleiben.
-                    </span>
-                    </label>
-                </div>
-            </div>
-
-            <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6">
-                <v-button @click.native="login">Einloggen</v-button>
-            </div>
+            </form>
         </auth-card>
     </guest-layout>
 </template>
