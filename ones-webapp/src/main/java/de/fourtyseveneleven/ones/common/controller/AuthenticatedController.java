f34112125d75473c21e85c8b09e6454aa0fe5601
Nikos Epping <nikos.epping@protonmail.com>
2021-04-30 21:14:04 +0200

    Use custom annotation for indicating a authenticated controller

--- ones-webapp/src/main/java/de/fourtyseveneleven/ones/common/controller/AuthenticatedController.java
+++
@@ -1,9 +0,0 @@
-package de.fourtyseveneleven.ones.common.controller;
-
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@SecurityRequirement(name = "bearerAuth")
-public abstract class AuthenticatedController {
-}
--- ones-webapp/src/main/java/de/fourtyseveneleven/ones/openapi/configuration/OpenApiConfiguration.java
+++ ones-webapp/src/main/java/de/fourtyseveneleven/ones/openapi/configuration/OpenApiConfiguration.java
@@ -6,14 +6,18 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
+import static de.fourtyseveneleven.ones.openapi.configuration.OpenApiConfiguration.BEARER_SECURITY_SCHEME_NAME;
+
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "Ones API", version = "v1"))
 @SecurityScheme(
-    name = "bearerAuth",
-    type = SecuritySchemeType.HTTP,
-    bearerFormat = "JWT",
-    scheme = "bearer"
+        name = BEARER_SECURITY_SCHEME_NAME,
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
 )
 public class OpenApiConfiguration {
+
+    public static final String BEARER_SECURITY_SCHEME_NAME = "bearerAuth";
 
 }
