a1014fa568a73e6f71268ecf6e042c3667359a15
Tim Vahlbrock <tim.vahlbrock@studmail.w-hs.de>
2021-05-10 16:34:32 +0200

    Contests-Store filter

--- ones-frontend/src/store/contests.vuex.ts
+++ ones-frontend/src/store/contests.vuex.ts
@@ -1,6 +1,16 @@
-import { action, createModule } from 'vuex-class-component';
+import { action, createModule, mutation } from 'vuex-class-component';
 import { ContestControllerApi, ContestDto as Contest } from '@/openapi/generated/api';
 import { vxm } from '.';
+
+interface FilterType {
+    titleContains?: string;
+    descriptionContains?: string;
+    startsBefore?: string;
+    startsAfter?: string;
+    endsBefore?: string;
+    endsAfter?: string;
+    organizerId?: number;
+}
 
 const VuexModule = createModule({
     namespaced: 'contests',
@@ -9,6 +19,7 @@ const VuexModule = createModule({
 
 export class ContestsStore extends VuexModule {
     private contests: Contest[] = [];
+    private filter = {} as FilterType;
 
     get all (): Contest[] {
         return this.contests;
@@ -19,7 +30,31 @@ export class ContestsStore extends VuexModule {
         const fetchResponse = await new ContestControllerApi({
             accessToken: vxm.user.token || '',
             isJsonMime: () => true
-        }).findAll();
+        }).findAll(...this.getFilterAsArray());
         this.contests = fetchResponse.data;
+    }
+
+    private getFilterAsArray (): Parameters<ContestControllerApi['findAll']> {
+        return [
+            this.filter.titleContains,
+            this.filter.descriptionContains,
+            this.filter.startsBefore,
+            this.filter.startsAfter,
+            this.filter.endsBefore,
+            this.filter.endsAfter,
+            this.filter.organizerId
+        ];
+    }
+
+    @mutation
+    addFilter (modification: FilterType): void {
+        this.filter = Object.assign(this.filter, modification);
+    }
+
+    @mutation
+    removeFilter (...filterPropsToClear: Array<keyof FilterType>): void {
+        for (const filterProp of filterPropsToClear) {
+            this.filter[filterProp] = undefined;
+        }
     }
 }
