3912297af82b42ab249633e2573f5469eb66d03b
Nikos Epping <nikos.epping@protonmail.com>
2021-05-14 19:19:31 +0200

    Return mock data in result api

--- ones-webapp/src/main/java/de/fourtyseveneleven/ones/results/controller/ResultController.java
+++ ones-webapp/src/main/java/de/fourtyseveneleven/ones/results/controller/ResultController.java
@@ -1,20 +1,24 @@
 package de.fourtyseveneleven.ones.results.controller;
 
 import de.fourtyseveneleven.ones.openapi.AuthenticatedApiController;
-import de.fourtyseveneleven.ones.results.model.ResultDto;
+import de.fourtyseveneleven.ones.results.model.ResultOverviewDto;
+import de.fourtyseveneleven.ones.results.service.ResultService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Collections;
-import java.util.List;
 
 @AuthenticatedApiController
 @RequestMapping("/api/v1/results")
 public class ResultController {
 
-    @GetMapping("/my")
-    public List<ResultDto> getMyResults() {
+    private final ResultService resultService;
 
-        return Collections.emptyList();
+    public ResultController(ResultService resultService) {
+        this.resultService = resultService;
+    }
+
+    @GetMapping("/my")
+    public ResultOverviewDto getMyResults() {
+
+        return resultService.getMyResults();
     }
 }
