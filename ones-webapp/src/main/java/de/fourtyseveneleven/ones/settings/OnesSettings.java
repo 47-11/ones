2c9b62def9966b878dedc62c53979f101765158c
Nikos Epping <nikos.epping@protonmail.com>
2021-06-07 21:34:30 +0200

    ⚙ Add settings for api authentification

--- ones-webapp/src/main/java/de/fourtyseveneleven/ones/settings/OnesSettings.java
+++ ones-webapp/src/main/java/de/fourtyseveneleven/ones/settings/OnesSettings.java
@@ -1,5 +1,6 @@
 package de.fourtyseveneleven.ones.settings;
 
+import de.fourtyseveneleven.ones.settings.ecm.EcmSettings;
 import de.fourtyseveneleven.ones.settings.message.MessageSettings;
 import de.fourtyseveneleven.ones.settings.security.SecuritySettings;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -14,11 +15,18 @@ public class OnesSettings {
     @NestedConfigurationProperty
     private final MessageSettings message = new MessageSettings();
 
+    @NestedConfigurationProperty
+    private final EcmSettings ecm = new EcmSettings();
+
     public SecuritySettings getSecurity() {
         return security;
     }
 
     public MessageSettings getMessage() {
         return message;
+    }
+
+    public EcmSettings getEcm() {
+        return ecm;
     }
 }
---
+++ ones-webapp/src/main/java/de/fourtyseveneleven/ones/settings/ecm/EcmSettings.java
@@ -0,0 +1,23 @@
+package de.fourtyseveneleven.ones.settings.ecm;
+
+public class EcmSettings {
+
+    private String apiUser;
+    private String apiPassword;
+
+    public String getApiUser() {
+        return apiUser;
+    }
+
+    public void setApiUser(String apiUser) {
+        this.apiUser = apiUser;
+    }
+
+    public String getApiPassword() {
+        return apiPassword;
+    }
+
+    public void setApiPassword(String apiPassword) {
+        this.apiPassword = apiPassword;
+    }
+}
