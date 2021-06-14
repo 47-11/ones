1ec21aa43a8b266a1bd4e2951af63e2a196122f7
Nikos Epping <nikos.epping@protonmail.com>
2021-06-14 21:57:02 +0200

    ⚙ Add ECM API impl for FullEventService

---
+++ ones-webapp/src/main/java/de/fourtyseveneleven/ones/event/service/impl/EcmApiFullEventServiceImpl.java
@@ -0,0 +1,47 @@
+package de.fourtyseveneleven.ones.event.service.impl;
+
+import de.fourtyseveneleven.ones.ecm.exception.EcmApiException;
+import de.fourtyseveneleven.ones.ecm.generated.ApiException;
+import de.fourtyseveneleven.ones.ecm.generated.api.EventContestControllerApi;
+import de.fourtyseveneleven.ones.ecm.generated.model.EventContest;
+import de.fourtyseveneleven.ones.event.mapper.FullEventMapper;
+import de.fourtyseveneleven.ones.event.model.dto.FullEventDto;
+import de.fourtyseveneleven.ones.event.service.FullEventService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static de.fourtyseveneleven.ones.message.MessageUtils.getExceptionMessage;
+
+@Service
+@Primary
+@Qualifier("ecmApiFullEventServiceImpl")
+public class EcmApiFullEventServiceImpl implements FullEventService {
+
+    private final EventContestControllerApi eventContestControllerApi;
+    private final FullEventMapper fullEventMapper;
+
+    public EcmApiFullEventServiceImpl(EventContestControllerApi eventContestControllerApi, FullEventMapper fullEventMapper) {
+        this.eventContestControllerApi = eventContestControllerApi;
+        this.fullEventMapper = fullEventMapper;
+    }
+
+    @Override
+    public Optional<FullEventDto> findOneByUuid(String eventUuid) {
+
+        try {
+            return tryFindOneByUuid(eventUuid);
+        } catch (ApiException e) {
+            throw new EcmApiException(getExceptionMessage("ecm.api-exception"), e);
+        }
+    }
+
+    private Optional<FullEventDto> tryFindOneByUuid(String eventUuid) throws ApiException {
+
+        final EventContest eventContest = eventContestControllerApi.getContestByUuid(eventUuid);
+        return Optional.ofNullable(eventContest)
+                .map(fullEventMapper::eventContestToFullEventDto);
+    }
+}
