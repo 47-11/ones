ab809acfa28a0c0e880981e98e96f20c9655afc2
Nikos Epping <nikos.epping@protonmail.com>
2021-06-30 11:21:40 +0200

    ⚙ Prevent NPE when mapping null OffsetDateTime

--- ones-webapp/src/main/java/de/fourtyseveneleven/ones/common/mapper/DateTimeFormatMapper.java
+++ ones-webapp/src/main/java/de/fourtyseveneleven/ones/common/mapper/DateTimeFormatMapper.java
@@ -5,11 +5,17 @@ import org.mapstruct.Mapper;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 
+import static java.util.Objects.isNull;
+
 @Mapper(componentModel = "spring")
 public interface DateTimeFormatMapper {
 
     default LocalDateTime mapOffsetDateTimeToLocalDateTime(OffsetDateTime offsetDateTime) {
 
-        return LocalDateTime.from(offsetDateTime);
+        if (isNull(offsetDateTime)) {
+            return null;
+        } else {
+            return LocalDateTime.from(offsetDateTime);
+        }
     }
 }
