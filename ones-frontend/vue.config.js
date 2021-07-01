9951113312b8c86ce3d27ad96cb63aa3b471c1ba
Tim Vahlbrock <tim.vahlbrock@studmail.w-hs.de>
2021-07-01 15:20:38 +0200

    ✨ Add basic vue pwa configuration

--- ones-frontend/vue.config.js
+++ ones-frontend/vue.config.js
@@ -2,7 +2,20 @@ module.exports = {
     outputDir: "target/classes/public",
     assetsDir: "assets",
     indexPath: "./index.html",
+    productionSourceMap: false,
     configureWebpack: {
         devtool: "source-map"
+    },
+    pwa: {
+        workboxPluginMode: "GenerateSW",
+        workboxOptions: {
+            skipWaiting: true
+        },
+        exclude: [
+            "/api/**"
+        ],
+        include: [
+            "/img/**"
+        ]
     }
 };
