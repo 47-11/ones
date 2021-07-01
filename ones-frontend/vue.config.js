e55557a40d048652f7ef284d57fdc49b4e7a1851
Tim Vahlbrock <tim.vahlbrock@studmail.w-hs.de>
2021-07-01 18:13:13 +0200

    ✨ List all icons in manifest

--- ones-frontend/vue.config.js
+++ ones-frontend/vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
             skipWaiting: true
         },
         themeColor: "#6366f1",
-        msTileColor: "#ffffff00",
+        backgroundColor: "#ffffff00",
         appleMobileWebAppCapable: true,
         iconPaths: {
             favicon32: "img/icons/favicon-32x32.png",
@@ -20,6 +20,82 @@ module.exports = {
             appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
             maskIcon: "img/icons/safari-pinned-tab.svg",
             msTileImage: "img/icons/icon-150x150.png"
+        },
+        manifestOptions: {
+            icons: [
+                {
+                    src: "./img/icons/android-chrome-192x192.png",
+                    sizes: "192x192",
+                    type: "image/png"
+                },
+                {
+                    src: "./img/icons/android-chrome-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png"
+                },
+                {
+                    src: "./img/icons/android-chrome-maskable-192x192.png",
+                    sizes: "192x192",
+                    type: "image/png",
+                    purpose: "maskable"
+                },
+                {
+                    src: "./img/icons/android-chrome-maskable-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png",
+                    purpose: "maskable"
+                },
+                {
+                    src: "./img/icons/apple-touch-icon-60x60.png",
+                    sizes: "60x60",
+                    type: "image/png"
+                },
+                {
+                    src: "./img/icons/apple-touch-icon-76x76.png",
+                    sizes: "76x76",
+                    type: "image/png"
+                },
+                {
+                    src: "./img/icons/apple-touch-icon-120x120.png",
+                    sizes: "120x120",
+                    type: "image/png"
+                },
+                {
+                    src: "./img/icons/apple-touch-icon-152x152.png",
+                    sizes: "152x152",
+                    type: "image/png"
+                },
+                {
+                    src: "./img/icons/apple-touch-icon-180x180.png",
+                    sizes: "180x180",
+                    type: "image/png"
+                },
+                {
+                    src: "./img/icons/apple-touch-icon.png",
+                    sizes: "180x180",
+                    type: "image/png"
+                },
+                {
+                    src: "./img/icons/favicon-16x16.png",
+                    sizes: "16x16",
+                    type: "image/png"
+                },
+                {
+                    src: "./img/icons/favicon-32x32.png",
+                    sizes: "32x32",
+                    type: "image/png"
+                },
+                {
+                    src: "./img/icons/msapplication-icon-144x144.png",
+                    sizes: "144x144",
+                    type: "image/png"
+                },
+                {
+                    src: "./img/icons/mstile-150x150.png",
+                    sizes: "150x150",
+                    type: "image/png"
+                }
+            ]
         }
     }
 };
