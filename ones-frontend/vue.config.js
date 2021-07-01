46cd2bfb7bd941c5dd32d18c762f5f0055fa52c9
Tim Vahlbrock <tim.vahlbrock@studmail.w-hs.de>
2021-07-01 17:00:22 +0200

    ✨ Exchange images, set theme and tile color

--- ones-frontend/vue.config.js
+++ ones-frontend/vue.config.js
@@ -11,11 +11,15 @@ module.exports = {
         workboxOptions: {
             skipWaiting: true
         },
-        exclude: [
-            "/api/**"
-        ],
-        include: [
-            "/img/**"
-        ]
+        themeColor: "#6366f1",
+        msTileColor: "#ffffff00",
+        appleMobileWebAppCapable: true,
+        iconPaths: {
+            favicon32: "img/icons/favicon-32x32.png",
+            favicon16: "img/icons/favicon-16x16.png",
+            appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
+            maskIcon: "img/icons/safari-pinned-tab.svg",
+            msTileImage: "img/icons/icon-150x150.png"
+        }
     }
 };
