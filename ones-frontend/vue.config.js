64efae6cb18c89b2570841983f635a2db46e5dae
Nikos Epping <nikos.epping@protonmail.com>
2021-07-01 13:23:21 +0200

    ⚙ Provide index.html as normal resource instead of view

--- ones-frontend/vue.config.js
+++ ones-frontend/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     outputDir: "target/classes/public",
     assetsDir: "assets",
-    indexPath: "../templates/index.html",
+    indexPath: "./index.html",
     configureWebpack: {
         devtool: "source-map"
     }
