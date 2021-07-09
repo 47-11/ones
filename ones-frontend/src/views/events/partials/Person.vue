0c75e888b9813f50636f60c5e4034a18df929800
Tim Vahlbrock <tim.vahlbrock@studmail.w-hs.de>
2021-07-09 09:53:33 +0200

    ✨ Only show phone and email icon if they are set

--- ones-frontend/src/views/events/partials/Person.vue
+++ ones-frontend/src/views/events/partials/Person.vue
@@ -10,7 +10,7 @@
         <br>
 
         <table class="mt-3">
-            <tr>
+            <tr v-if="person.phoneNumber">
                 <td>
                     <font-awesome-icon :icon="'phone'" class="text-gray-400"/>
                 </td>
@@ -18,7 +18,7 @@
                     {{ person.phoneNumber }}
                 </td>
             </tr>
-            <tr>
+            <tr v-if="person.emailAddress">
                 <td>
                     <font-awesome-icon :icon="'at'" class="text-gray-400"/>
                 </td>
