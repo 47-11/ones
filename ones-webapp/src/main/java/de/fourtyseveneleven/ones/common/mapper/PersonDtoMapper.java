b549c3c7a04dba25d7a63de0d7fc955a9a17442e
Nikos Epping <nikos.epping@protonmail.com>
2021-10-13 09:56:26 +0200

    ⚙ Remove unused mappings from PersonDtoMapper

--- ones-webapp/src/main/java/de/fourtyseveneleven/ones/common/mapper/PersonDtoMapper.java
+++ ones-webapp/src/main/java/de/fourtyseveneleven/ones/common/mapper/PersonDtoMapper.java
@@ -1,23 +1,15 @@
 package de.fourtyseveneleven.ones.common.mapper;
 
 import de.fourtyseveneleven.ones.common.model.dto.AddressDto;
-import de.fourtyseveneleven.ones.common.model.dto.PersonDto;
-import de.fourtyseveneleven.ones.ecm.generated.ApiException;
 import de.fourtyseveneleven.ones.ecm.generated.api.MasterdataContactControllerApi;
 import de.fourtyseveneleven.ones.ecm.generated.model.MasterdataContact;
 import de.fourtyseveneleven.ones.ecm.generated.model.MasterdataContactAddress;
 import de.fourtyseveneleven.ones.user.model.dto.UserDto;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.Named;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-
-import static java.text.MessageFormat.format;
-import static org.apache.commons.lang3.StringUtils.isBlank;
 
 @Mapper(componentModel = "spring", uses = {CommonMapper.class})
 public abstract class PersonDtoMapper {
@@ -25,28 +17,13 @@ public abstract class PersonDtoMapper {
     @Autowired
     protected MasterdataContactControllerApi masterdataContactControllerApi;
 
-    @Named(value = "contactUuidToPersonDto")
-    public PersonDto contactUuidToPersonDto(String contactUuid) throws ApiException {
-
-        if (isBlank(contactUuid)) {
-            return null;
-        } else {
-            return masterdataContactToUserDto(masterdataContactControllerApi.getContactByUuid(contactUuid));
-        }
-    }
 
     @Mapping(source = "addresses", target = "address")
     @Mapping(source = "forename", target = "firstName")
     @Mapping(source = "surname", target = "lastName")
     public abstract UserDto masterdataContactToUserDto(MasterdataContact masterdataContact);
 
-    @Mapping(source = "addresses", target = "address")
-    @Mapping(source = "forename", target = "firstName")
-    @Mapping(source = "surname", target = "lastName")
-    public abstract PersonDto masterdataContactToPersonDto(MasterdataContact masterdataContact);
-
-
-    public AddressDto masterdataContactAddressesToAddressDto(List<MasterdataContactAddress> masterdataContactAddresses) {
+    protected AddressDto masterdataContactAddressesToAddressDto(List<MasterdataContactAddress> masterdataContactAddresses) {
 
         if (masterdataContactAddresses.isEmpty()) {
             return null;
@@ -58,11 +35,4 @@ public abstract class PersonDtoMapper {
     @Mapping(source = "zipcodeCode", target = "zipCode")
     @Mapping(source = "countryCode", target = "country")
     public abstract AddressDto masterdataContactAddressToAddressDto(MasterdataContactAddress masterdataContactAddress);
-
-    @AfterMapping
-    public void afterMasterdataContactAddressToAddressDto(@MappingTarget AddressDto addressDto, MasterdataContactAddress masterdataContactAddress) {
-
-        final String street = format("{0} {1}", masterdataContactAddress.getStreet(), masterdataContactAddress.getHouseNumber());
-        addressDto.setStreet(street);
-    }
 }
