f14be3ef23e6726dc70f24c0890cdfd6721dff27
Tim Vahlbrock <tim.vahlbrock@studmail.w-hs.de>
2021-10-26 17:22:29 +0200

    Add node script to generate api

---
+++ ones-frontend/generateApi.js
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const http = require("http");
+const fs = require("fs");
+const child = require("child_process");
+
+console.log("(1) Requesting data. Make sure backend is serving api definition at http://localhost:8080/api/docs/v3");
+http.get("http://localhost:8080/api/docs/v3", (res) => {
+    res.setEncoding("utf8");
+    let rawData = "";
+    res.on("data", (chunk) => { rawData += chunk; });
+    res.on("end", () => {
+        console.log("(2) Finished reading data.");
+        try {
+            const prettyData = JSON.stringify(JSON.parse(rawData), null, 4);
+            fs.writeFileSync("./src/openapi/api-docs.json", prettyData);
+            console.log("(3) Wrote api-docs.json.");
+            child.execSync("mvn generate-resources -DskipCi", {stdio: "inherit"});
+            console.log("(4) Finished generating.");
+            console.log("(5) Done.");
+        } catch (e) {
+            console.error(e);
+        }
+    });
+});
