cad9d90cf16939f28368e046ac5faba62e10a265
Tim Vahlbrock <tim.vahlbrock@studmail.w-hs.de>
2021-11-02 14:44:22 +0100

    Improved errors in addHorse

--- ones-frontend/src/views/horses/modals/addHorse/VAddHorseContent.vue
+++ ones-frontend/src/views/horses/modals/addHorse/VAddHorseContent.vue
@@ -7,24 +7,24 @@
             </div>
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.horse.name") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('name', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateHorse('name', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-6 sm:invisible"></div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.horse.passportNumber") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('equinePassportNo', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateHorse('passportNumber', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.horse.chipNumber") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('chipNo', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateHorse('chipNumber', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.horse.gender") }}</v-label>
-                <v-select :value="gender" type="text" class="w-full" @input="update('gender', $event)" :disabled="inputsDisabled">
+                <v-select :value="horse.gender" type="text" class="w-full" @input="updateHorse('gender', $event)" :disabled="inputsDisabled">
                     <option v-for="(gender, index) in genders" :key="index" :value="gender">
                         {{ $t("data.horse.genderOptions." + gender) }}
                     </option>
@@ -33,27 +33,27 @@
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.horse.breed") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('breed', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateHorse('breed', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.horse.color") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('color', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateHorse('color', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.horse.yearOfBirth") }}</v-label>
-                <v-input :value="yearOfBirth" type="number" class="w-full" @input="update('yearOfBirth', $event)" :disabled="inputsDisabled"/>
+                <v-input type="number" class="w-full" @input="updateHorse('yearOfBirth', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.horse.size") }}</v-label>
-                <v-input :value="size" type="number" class="w-full" @input="update('size', $event)" :disabled="inputsDisabled"/>
+                <v-input type="number" class="w-full" @input="updateHorse('size', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.horse.feiNo") }}</v-label>
-                <v-input :value="size" type="number" class="w-full" @input="update('feiNo', $event)" :disabled="inputsDisabled"/>
+                <v-input type="number" class="w-full" @input="updateHorse('feiNumber', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 mt-8">
@@ -61,27 +61,27 @@
             </div>
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.address.street") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('stableStreet', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateStable('street', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.address.houseNumber") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('stableHouseNumber', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateStable('houseNumber', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.address.zipCode") }}</v-label>
-                <v-input type="number" class="w-full" @input="update('stableZipCode', $event)" :disabled="inputsDisabled"/>
+                <v-input type="number" class="w-full" @input="updateStable('zipCode', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.address.city") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('stableCity', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateStable('city', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-6">
                 <v-label>{{ $t("data.address.country") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('stableCountry', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateStable('country', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 mt-8">
@@ -89,47 +89,47 @@
             </div>
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.user.firstName") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('ownerFirstName', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateOwner('firstName', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.user.lastName") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('ownerLastName', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateOwner('lastName', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.user.phoneNumber") }}</v-label>
-                <v-input type="tel" class="w-full" @input="update('ownerPhoneNumber', $event)" :disabled="inputsDisabled"/>
+                <v-input type="tel" class="w-full" @input="updateOwner('phoneNumber', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.user.phoneNumberMobile") }}</v-label>
-                <v-input type="tel" class="w-full" @input="update('ownerMobileNumber', $event)" :disabled="inputsDisabled"/>
+                <v-input type="tel" class="w-full" @input="updateOwner('phoneNumberMobile', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.address.street") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('ownerStreet', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateOwnerAddress('street', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.address.houseNumber") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('ownerHouseNumber', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateOwnerAddress('houseNumber', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.address.zipCode") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('ownerZipCode', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateOwnerAddress('zipCode', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-12 sm:col-span-6">
                 <v-label>{{ $t("data.address.city") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('ownerCity', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateOwnerAddress('city', $event)" :disabled="inputsDisabled"/>
             </div>
 
             <div class="col-span-6">
                 <v-label>{{ $t("data.address.country") }}</v-label>
-                <v-input type="text" class="w-full" @input="update('ownerCountry', $event)" :disabled="inputsDisabled"/>
+                <v-input type="text" class="w-full" @input="updateOwnerAddress('country', $event)" :disabled="inputsDisabled"/>
             </div>
         </div>
 
@@ -143,7 +143,7 @@ import VInput from "@/components/forms/VInput.vue";
 import VSelect from "@/components/forms/VSelect.vue";
 import VLabel from "@/components/forms/VLabel.vue";
 import { vxm } from "@/store";
-import { HorseDtoGenderEnum } from "@/openapi/generated";
+import { AddressDto, HorseDto, HorseDtoGenderEnum, PersonDto } from "@/openapi/generated";
 
 @Component({
     components: {
@@ -163,74 +163,42 @@ export default class VAddHorseContent extends Vue {
     inputsDisabled = false;
     error: Error | null = null;
 
-    name = "";
-    equinePassportNo = "";
-    chipNo = "";
-    gender: HorseDtoGenderEnum = HorseDtoGenderEnum.Female;
-    breed = "";
-    color = "";
-    yearOfBirth: number | null = null;
-    size: number | null = null;
-    feiNo = "";
-
-    stableStreet = "";
-    stableHouseNumber = "";
-    stableZipCode = "";
-    stableCity = "";
-    stableCountry = "";
-
-    ownerFirstName = "";
-    ownerLastName = "";
-    ownerPhoneNumber = "";
-    ownerMobileNumber = "";
-    ownerStreet = "";
-    ownerHouseNumber = "";
-    ownerZipCode = "";
-    ownerCity = "";
-    ownerCountry = "";
+    horse: HorseDto = {
+        gender: HorseDtoGenderEnum.Female,
+        stableAddress: {},
+        owner: {
+            address: { }
+        }
+    };
 
     genders = HorseDtoGenderEnum;
 
-    public update(property: keyof this, value: string): void {
+    public updateHorse(property: keyof HorseDto, value: string): void {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        this[property] = value as any;
+        this.horse[property] = value as any;
+    }
+
+    public updateStable(property: keyof AddressDto, value: string): void {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        this.horse.stableAddress![property] = value;
+    }
+
+    public updateOwner(property: keyof PersonDto, value: string): void{
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        this.horse.owner![property] = value;
+    }
+
+    public updateOwnerAddress(property: keyof AddressDto, value: string): void {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        this.horse.owner!.address![property] = value;
     }
 
     private async setData(): Promise<void> {
         try {
             this.assertRequiredFilled();
             this.assertAllInRange();
-            await vxm.horses.add({
-                passportNumber: this.equinePassportNo,
-                chipNumber: this.chipNo,
-                name: this.name,
-                gender: this.gender,
-                breed: this.breed,
-                color: this.color,
-                size: this.size || 0,
-                yearOfBirth: this.yearOfBirth || 0,
-                // feiNo: this.feiNo, Needs to be added when available
-                stableAddress: {
-                    street: this.stableStreet,
-                    houseNumber: this.stableHouseNumber,
-                    zipCode: this.stableZipCode,
-                    city: this.stableCity,
-                    country: this.stableCountry
-                },
-                owner: {
-                    firstName: this.ownerFirstName,
-                    lastName: this.ownerLastName,
-                    phoneNumber: this.ownerPhoneNumber,
-                    phoneNumberMobile: this.ownerMobileNumber,
-                    address: {
-                        street: this.ownerStreet,
-                        houseNumber: this.ownerHouseNumber,
-                        zipCode: this.ownerZipCode,
-                        city: this.ownerCity,
-                        country: this.ownerCountry
-                    }
-                }
-            });
+
+            await vxm.horses.add(this.horse);
             this.$vfm.hide("add-horse");
         } catch (error) {
             this.error = error as Error;
@@ -238,48 +206,78 @@ export default class VAddHorseContent extends Vue {
     }
 
     assertRequiredFilled(): void {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const checkValue = (value: any) => {
+            return !value || (typeof value === "string" && value.length === 0);
+        };
+        const notFilled = (propNameKey: string) => {
+            return new Error(this.$t(propNameKey) + this.$t("horses.modals.add.errors.notFilled").toString());
+        };
+
         for (const prop of this.requiredProps) {
-            const value = this[prop];
-            if (!value || (typeof value === "string" && value.length === 0)) {
-                throw new Error(prop + this.$t("horses.modals.add.errors.notFilled").toString());
+            if (checkValue(this.horse[prop])) {
+                throw notFilled("data.horse." + prop);
+            }
+        }
+
+        for (const prop of this.requiredStableProps) {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            if (checkValue(this.horse.stableAddress![prop])) {
+                throw notFilled("data.address." + prop);
+            }
+        }
+
+        for (const prop of this.requiredOwnerProps) {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            if (checkValue(this.horse.owner![prop])) {
+                throw notFilled("data.user." + prop);
+            }
+        }
+
+        for (const prop of this.requiredOwnerAddressProps) {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            if (checkValue(this.horse.owner!.address![prop])) {
+                throw notFilled("data.address." + prop);
             }
         }
     }
 
-    get requiredProps(): Array<keyof this> {
-        return [
-            "name",
-            "equinePassportNo",
-            "chipNo",
-            "gender",
-            "breed",
-            "color",
-            "yearOfBirth",
-            "size",
-            "feiNo",
-            "stableStreet",
-            "stableHouseNumber",
-            "stableZipCode",
-            "stableCity",
-            "stableCountry",
-            "ownerFirstName",
-            "ownerLastName",
-            "ownerPhoneNumber",
-            "ownerMobileNumber",
-            "ownerStreet",
-            "ownerHouseNumber",
-            "ownerZipCode",
-            "ownerCity",
-            "ownerCountry"
-        ];
-    }
+    requiredProps: Array<keyof HorseDto> = [
+        "name",
+        "passportNumber",
+        "chipNumber",
+        "gender",
+        "breed",
+        "color",
+        "yearOfBirth",
+        "size",
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        "feiNumber" as any
+    ];
+
+    requiredStableProps: Array<keyof AddressDto> = [
+        "street",
+        "houseNumber",
+        "zipCode",
+        "city",
+        "country"
+    ];
+
+    requiredOwnerProps: Array<keyof PersonDto> = [
+        "firstName",
+        "lastName",
+        "phoneNumber",
+        "phoneNumberMobile"
+    ];
+
+    requiredOwnerAddressProps = this.requiredStableProps;
 
     assertAllInRange(): void {
         const currentYear = new Date().getFullYear();
-        if (this.yearOfBirth && (this.yearOfBirth < (currentYear - 60) || this.yearOfBirth > currentYear)) {
+        if (this.horse.yearOfBirth && (this.horse.yearOfBirth < (currentYear - 60) || this.horse.yearOfBirth > currentYear)) {
             throw new Error(this.$t("horses.modals.add.errors.yearInvalid").toString());
         }
-        if (this.size && (this.size < 40 || this.size > 250)) {
+        if (this.horse.size && (this.horse.size < 40 || this.horse.size > 250)) {
             throw new Error(this.$t("horses.modals.add.errors.sizeOutOfRange").toString());
         }
     }
