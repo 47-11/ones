2721c11d97153e6a8fc48f24119934083044ad6f
Nikos Epping <nikos.epping@protonmail.com>
2021-11-19 17:55:59 +0100

    Add mapping for horse gender

--- ones-webapp/src/main/java/de/fourtyseveneleven/ones/horse/HorseDtoMapper.java
+++ ones-webapp/src/main/java/de/fourtyseveneleven/ones/horse/HorseDtoMapper.java
@@ -6,23 +6,28 @@ import de.fourtyseveneleven.ones.common.model.dto.AddressDto;
 import de.fourtyseveneleven.ones.ecm.generated.model.RegisterContactLegal;
 import de.fourtyseveneleven.ones.ecm.generated.model.RegisterHorse;
 import de.fourtyseveneleven.ones.ecm.generated.model.RegisteredHorse;
+import de.fourtyseveneleven.ones.horse.model.Gender;
 import de.fourtyseveneleven.ones.horse.model.HorseDto;
 import de.fourtyseveneleven.ones.user.mapper.UserDtoMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import static de.fourtyseveneleven.ones.ecm.generated.model.RegisterHorse.GenderEnum.G;
+import static de.fourtyseveneleven.ones.ecm.generated.model.RegisterHorse.GenderEnum.M;
+import static de.fourtyseveneleven.ones.ecm.generated.model.RegisterHorse.GenderEnum.S;
+import static de.fourtyseveneleven.ones.horse.model.Gender.GELDING;
+import static de.fourtyseveneleven.ones.horse.model.Gender.MARE;
+import static de.fourtyseveneleven.ones.horse.model.Gender.STALLION;
 
 @Mapper(componentModel = "spring", uses = {CommonMapper.class, UserDtoMapper.class, AddressDtoMapper.class})
 public abstract class HorseDtoMapper {
 
-    // TODO: Map gender
     @Mapping(source = "passportNumber", target = "passportId")
     @Mapping(source = "chipNumber", target = "chipId")
     @Mapping(source = "size", target = "stickSize")
     @Mapping(source = "stableAddress", target = "stable.postalAddress")
     public abstract RegisterHorse horseDtoToRegisterHorse(HorseDto horseDto);
 
-    // TODO: Map gender
     @Mapping(source = "passportId", target = "passportNumber")
     @Mapping(source = "chipId", target = "chipNumber")
     @Mapping(source = "stickSize", target = "size")
@@ -30,4 +35,22 @@ public abstract class HorseDtoMapper {
     public abstract HorseDto registeredHorseToHorseDto(RegisteredHorse masterdataHorse);
 
     protected abstract AddressDto registerContactLegalToAddressDto(RegisterContactLegal registerContactLegal);
+
+    protected RegisterHorse.GenderEnum mapGender(Gender gender) {
+
+        return switch (gender) {
+            case GELDING -> G;
+            case MARE -> M;
+            case STALLION -> S;
+        };
+    }
+
+    protected Gender mapGender(String genderText) {
+
+        return switch (RegisterHorse.GenderEnum.fromValue(genderText)) {
+            case G -> GELDING;
+            case M -> MARE;
+            case S -> STALLION;
+        };
+    }
 }
