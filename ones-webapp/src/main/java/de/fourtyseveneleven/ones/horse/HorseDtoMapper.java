6512134fe2284f8aa915769705dc1ba8d3d11e40
Nikos Epping <nikos.epping@protonmail.com>
2021-11-19 17:51:18 +0100

    Remove unused mapping method

--- ones-webapp/src/main/java/de/fourtyseveneleven/ones/horse/HorseDtoMapper.java
+++ ones-webapp/src/main/java/de/fourtyseveneleven/ones/horse/HorseDtoMapper.java
@@ -3,13 +3,14 @@ package de.fourtyseveneleven.ones.horse;
 import de.fourtyseveneleven.ones.common.mapper.AddressDtoMapper;
 import de.fourtyseveneleven.ones.common.mapper.CommonMapper;
 import de.fourtyseveneleven.ones.common.model.dto.AddressDto;
-import de.fourtyseveneleven.ones.ecm.generated.model.*;
+import de.fourtyseveneleven.ones.ecm.generated.model.RegisterContactLegal;
+import de.fourtyseveneleven.ones.ecm.generated.model.RegisterHorse;
+import de.fourtyseveneleven.ones.ecm.generated.model.RegisteredHorse;
 import de.fourtyseveneleven.ones.horse.model.HorseDto;
 import de.fourtyseveneleven.ones.user.mapper.UserDtoMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import java.util.Optional;
 
 @Mapper(componentModel = "spring", uses = {CommonMapper.class, UserDtoMapper.class, AddressDtoMapper.class})
 public abstract class HorseDtoMapper {
@@ -27,13 +28,6 @@ public abstract class HorseDtoMapper {
     @Mapping(source = "stickSize", target = "size")
     @Mapping(source = "stable", target = "stableAddress")
     public abstract HorseDto registeredHorseToHorseDto(RegisteredHorse masterdataHorse);
-
-    protected String mapMasterdataPropertyCharacteristic(MasterdataPropertyCharacteristic masterdataPropertyCharacteristic) {
-
-        return Optional.ofNullable(masterdataPropertyCharacteristic)
-                .map(MasterdataPropertyCharacteristic::getDisplay)
-                .orElse(null);
-    }
 
     protected abstract AddressDto registerContactLegalToAddressDto(RegisterContactLegal registerContactLegal);
 }
