3eb361106284d4e6127b974d316ddbca44df66b6
Nikos Epping <nikos.epping@protonmail.com>
2021-12-07 11:14:18 +0100

    Provide mock results

--- ones-webapp/src/main/java/de/fourtyseveneleven/ones/results/controller/ResultController.java
+++ ones-webapp/src/main/java/de/fourtyseveneleven/ones/results/controller/ResultController.java
@@ -1,10 +1,18 @@
 package de.fourtyseveneleven.ones.results.controller;
 
+import de.fourtyseveneleven.ones.event.model.dto.SimpleContestDto;
+import de.fourtyseveneleven.ones.event.model.dto.SimpleEventDto;
+import de.fourtyseveneleven.ones.horse.model.SimpleHorseDto;
 import de.fourtyseveneleven.ones.openapi.AuthenticatedApiController;
+import de.fourtyseveneleven.ones.results.model.ResultDto;
 import de.fourtyseveneleven.ones.results.model.ResultOverviewDto;
 import de.fourtyseveneleven.ones.results.service.ResultService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.UUID;
 
 @AuthenticatedApiController
 @RequestMapping("/api/v1/results")
@@ -19,6 +27,16 @@ public class ResultController {
     @GetMapping("/my")
     public ResultOverviewDto getMyResults() {
 
-        return resultService.getMyResults();
+        final SimpleHorseDto horse = new SimpleHorseDto(UUID.randomUUID(), "Shadowfax");
+        final ResultDto resultDto = new ResultDto(new SimpleContestDto(),
+                new SimpleEventDto(),
+                Collections.singletonList(horse),
+                3,
+                7671246,
+                BigDecimal.TEN,
+                BigDecimal.TEN,
+                null);
+
+        return new ResultOverviewDto(Collections.singletonList(resultDto));
     }
 }
